87e63d02208aea466c6413fb545962617d5e4485
ilarioforcherio <ilarioforcherio@gmail.com>
2022-10-19 16:40:59 +0200

    ragionamento javascript + esecuzione

---
+++ assets/js/main.js
@@ -0,0 +1,81 @@
+
+
+
+/*Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+-il prezzo del biglietto è definito in base ai km (0.21 € al km)
+-va applicato uno sconto del 20% per i minorenni
+-va applicato uno sconto del 40% per gli over 65.
+-L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.*/
+
+
+// chiedere all'utente l'eta
+//chiedere all'utente quanti km
+// moltiplicare prezzo (0.21) per i km 
+// variabile per lo sconto 20%
+// variabile per lo sconto 40%
+// if < 18
+// if > 18
+
+
+
+
+
+
+//età utente
+let etaUtente = parseInt(prompt('inserisci la tua età'));
+console.log(etaUtente);
+
+//kilometri
+let kmUtente = parseInt(prompt('inserisci quanti km vuoi fare'));
+console.log(kmUtente);
+
+//prezzo ticket
+
+let prezzoTicket = kmUtente * 0.21;
+console.log(prezzoTicket);
+
+let sconto20 = prezzoTicket * 100 / 20;
+
+let sconto40 = prezzoTicket * 100 / 40; 
+
+//condizioni
+//18 anni
+if (etaUtente < 18){
+    prezzoTicket = prezzoTicket - sconto20;
+    
+//65 anni
+}else if (etaUtente > 65){
+    prezzoTicket = prezzoTicket - sconto40;
+    
+//altre età
+}else {
+    prezzoTicket = prezzoTicket;
+}
+
+// document.getElementById("biglietto").innerHTML = "il tuo biglietto è "  + prezzoTicket.toFixed(2) + " euro";
+
+
+document.getElementById("ilTuoBiglietto").innerHTML= `Il prezzo del biglietto è: ${prezzoTicket.toFixed(2)} Euro`;
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
